99baf7ece7233a2e15935a27168efdd2c6f7acab
Evan J <42357644+ejach@users.noreply.github.com>
2020-11-18 09:51:38 -0500

    Update plex-motd.py
    Added colors and a ternary operator that corrects the grammar if more than one session is currently present in the 'sessions' array.

--- plex-motd.py
+++ plex-motd.py
@@ -2,10 +2,17 @@
 
 import urllib.request
 import xml.etree.ElementTree as ET
+from colored import fg, bg, attr, stylize
 
 server = 'localhost'
 port = '32400'
 plex_token = ''
+#----------------COLORS-----------------
+white = fg('white')
+yellow = fg('yellow')
+green = fg('green')
+red = fg('red')
+reset = attr('reset')
 #---------------------------------------
 
 # session array
@@ -40,10 +47,11 @@ for item in root.findall('./Video'):
 			session[child.tag] = child.attrib['title']
 
 	sessions.append(session)
-
-print ("There are currently {0} sessions:\n".format(len(sessions)))
+print (stylize("CURRENT ", white) + stylize("PLEX ", yellow) + stylize("SESSIONS", white))
+sessionNum = len(sessions)
+print (stylize("Currently, ", white) + stylize('0', red) + stylize(" users are in a session.", white) if not sessions else stylize("Currently, ", white) + stylize(sessionNum, green) +  stylize(" user(s) are in a session:", white))
 for session in sessions:
 	if session['Type'] == 'episode':
-		print ('* {0} is watching "{1} - S{2}E{3} - {4}" on {5}'.format(session['User'], session['Series'], session['Season'], session['Episode'], session['Title'], session['Player']))
+		print (white + '- {0} is watching "{1} - S{2}E{3} - {4}" on {5}'.format(session['User'], session['Series'], session['Season'], session['Episode'], session['Title'], session['Player']) + reset)
 	else:
-		print ('* {0} is watching "{1}" on {2}'.format(session['User'], session['Title'], session['Player']))
+		print (white + '- {0} is watching "{1}" on {2}'.format(session['User'], session['Title'], session['Player']) + reset)
