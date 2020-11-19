3b0156c7f60b6dbcb9401a1b37be436cc8dec36f
Evan J <42357644+ejach@users.noreply.github.com>
2020-11-19 11:17:43 -0500

    Update plex-motd.py
    Added another change in grammar if just a single user is currently in a session.

--- plex-motd.py
+++ plex-motd.py
@@ -6,7 +6,7 @@ from colored import fg, bg, attr, stylize
 
 server = 'localhost'
 port = '32400'
-plex_token = ''
+plex_token = 'kC1Ec3FnF3MWWGAtC37o'
 #----------------COLORS-----------------
 white = fg('white')
 yellow = fg('yellow')
@@ -49,7 +49,11 @@ for item in root.findall('./Video'):
 	sessions.append(session)
 print (stylize("CURRENT ", white) + stylize("PLEX ", yellow) + stylize("SESSIONS", white))
 sessionNum = len(sessions)
-print (stylize("Currently, ", white) + stylize('0', red) + stylize(" users are in a session.", white) if not sessions else stylize("Currently, ", white) + stylize(sessionNum, green) +  stylize(" user(s) are in a session:", white))
+if sessionNum == 1:
+	print (stylize("Currently, ", white) + stylize(sessionNum, green) +  stylize(" user is in a session:", white))
+else:
+	print (stylize("Currently, ", white) + stylize('0', red) + stylize(" users are in a session.", white) if not sessions else stylize("Currently, ", white) + stylize(sessionNum, green) +  stylize(" users are in a session:", white))
+
 for session in sessions:
 	if session['Type'] == 'episode':
 		print (white + '- {0} is watching "{1} - S{2}E{3} - {4}" on {5}'.format(session['User'], session['Series'], session['Season'], session['Episode'], session['Title'], session['Player']) + reset)
